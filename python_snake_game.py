863335f0658660ef20df7581d968e4fcf6448fcc
victorcynzo <vynzonides@gmail.com>
2024-06-16 14:06:18 +0200

    Added Game Over Screen

--- python_snake_game.py
+++ python_snake_game.py
@@ -126,7 +126,9 @@ def check_collisions(snake):
         return False
 
 def game_over():
-    pass
+    canvas.delete(ALL)
+    canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2, font=('consolas', 70), text='Game Over', fill="red", tag="game_over")
+    
 
 window = Tk()
 window.title("Snek Game")
