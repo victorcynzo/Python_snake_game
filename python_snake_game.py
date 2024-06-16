55f0842c07aa0dc78659c12dee136722f54f3644
victorcynzo <vynzonides@gmail.com>
2024-06-16 12:55:39 +0200

    First Window with Score

--- python_snake_game.py
+++ python_snake_game.py
@@ -1,2 +1,43 @@
 from tkinter import *
 import random
+
+#constant variables
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE =  50
+BODY_PARTS =  3
+SNAKE_COLOR = "green"
+FOOD_COLOR = "red"
+BACKGROUND_COLOR = "black"
+
+
+# class_Snake:
+#    pass
+
+# class_Food:
+#    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+window.title("Snek Game")
+window.resizable(False, False)
+
+score = 0
+direction = 'down'
+
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+label.pack()
+
+window.mainloop()
