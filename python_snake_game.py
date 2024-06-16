3c7782c3559687c5d48aae9cd842b349d6168ece
victorcynzo <vynzonides@gmail.com>
2024-06-16 13:54:51 +0200

    Snake Movement

--- python_snake_game.py
+++ python_snake_game.py
@@ -68,7 +68,24 @@ def next_turn(snake, food):
     window.after(SPEED, next_turn, snake, food)
 
 def change_direction(new_direction):
-    pass
+    
+    global direction
+
+    if new_direction == 'left':
+        if direction != 'right':
+            direction = new_direction
+
+    elif new_direction == 'right':
+        if direction != 'left':
+            direction = new_direction
+    
+    elif new_direction == 'up':
+        if direction != 'down':
+            direction = new_direction
+
+    elif new_direction == 'down':
+        if direction != 'up':
+            direction = new_direction
 
 def check_collisions():
     pass
@@ -100,6 +117,11 @@ x = int((screen_width/2) - (screen_width/2))
 y = int((screen_height/2) - (screen_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
 
 snake = Snake()
 food = Food()
